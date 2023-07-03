c67628c376b1935102df9c927395a16af937b712
kosimbekGH <69970961+kosimbekGH@users.noreply.github.com>
2023-07-03 12:15:15 +0500

    2 files modified, 2 added, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './components/app/app.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppComponent],
   templateUrl: './main.html'
 })
 export class App {
   name = 'Angular';
+
+
+
 }
 
 bootstrapApplication(App);
